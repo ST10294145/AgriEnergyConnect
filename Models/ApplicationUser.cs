93735b17e2242278546681e8cbcbd8376dffdfa2
Saihil Gurupersad <ST10294145@vcconnect.edu.za>
2025-06-17 13:40:22 +0200

    Created Application User model class

---
+++ Models/ApplicationUser.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace AgriEnergyConnect.Models
+{
+    public class ApplicationUser : IdentityUser
+    {
+        public string Role { get; set; }              // "Employee" or "Farmer"
+        public string? CreatedByEmployeeId { get; set; }  // For farmers, links to their creator
+    }
+}
+
+
+
+
+
