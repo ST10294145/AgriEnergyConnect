a6c9e7c2304e189e4f651ddc5a9f790b151199d8
Saihil Gurupersad <ST10294145@vcconnect.edu.za>
2025-06-17 18:12:20 +0200

    Created a controller for employees

---
+++ Controllers/EmployeeController.cs
@@ -0,0 +1,64 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Threading.Tasks;
+using AgriEnergyConnect.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AgriEnergyConnect.Controllers
+{
+    [Authorize(Roles = "Employee")]
+    public class EmployeeController : Controller
+    {
+        private readonly UserManager<ApplicationUser> _userManager;
+
+        public EmployeeController(UserManager<ApplicationUser> userManager)
+        {
+            _userManager = userManager;
+        }
+
+        public IActionResult RegisterFarmer()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> RegisterFarmer(RegisterFarmerViewModel model)
+        {
+            if (!ModelState.IsValid)
+                return View(model);
+
+            var user = new ApplicationUser { UserName = model.Email, Email = model.Email };
+            var result = await _userManager.CreateAsync(user, model.Password);
+
+            if (result.Succeeded)
+            {
+                await _userManager.AddToRoleAsync(user, "Farmer");
+                ViewBag.Message = "Farmer account created successfully.";
+                return View(); // Return same form with success message
+            }
+
+            foreach (var error in result.Errors)
+                ModelState.AddModelError(string.Empty, error.Description);
+
+            return View(model);
+        }
+    }
+
+    public class RegisterFarmerViewModel
+    {
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+
+        [Required]
+        [StringLength(100, MinimumLength = 6)]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        [Compare("Password", ErrorMessage = "Passwords do not match.")]
+        public string ConfirmPassword { get; set; }
+    }
+}
