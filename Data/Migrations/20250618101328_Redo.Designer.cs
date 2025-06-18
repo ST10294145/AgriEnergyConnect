545f90236e4075a2a13743bd4e8fcff2130188cc
Saihil Gurupersad <ST10294145@vcconnect.edu.za>
2025-06-18 12:28:18 +0200

    Fixed Namespace for migration file

--- Data/Migrations/20250618101328_Redo.Designer.cs
+++ Data/Migrations/20250618101328_Redo.Designer.cs
@@ -9,7 +9,7 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
 
-namespace AgriEnergyConnect.Migrations
+namespace AgriEnergyConnect.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
     [Migration("20250618101328_Redo")]
