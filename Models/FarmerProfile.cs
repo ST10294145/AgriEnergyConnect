e233ee4fa5b374dae8fcbe23b6bed97b42357e9f
Saihil Gurupersad <ST10294145@vcconnect.edu.za>
2025-06-19 13:27:55 +0200

    Created new model class for Farmer Profiles

---
+++ Models/FarmerProfile.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace AgriEnergyConnect.Models
+{
+    public class FarmerProfile
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int FarmerId { get; set; }
+
+        [Required]
+        public string UserId { get; set; }
+
+        [ForeignKey("UserId")]
+        public ApplicationUser User { get; set; }
+
+        [Required]
+        [StringLength(100)]
+        public string Name { get; set; }
+
+        [Required]
+        [StringLength(100)]
+        public string Location { get; set; }
+    }
+}
