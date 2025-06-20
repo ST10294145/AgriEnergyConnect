5c2184c6feb94afb287038975becec24b270d3ca
Saihil Gurupersad <ST10294145@vcconnect.edu.za>
2025-06-20 15:24:31 +0200

    Updated controller

--- Controllers/ProductController.cs
+++ Controllers/ProductController.cs
@@ -1,12 +1,83 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using AgriEnergyConnect.Data;
+using AgriEnergyConnect.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Linq;
+using System.Threading.Tasks;
 
 namespace AgriEnergyConnect.Controllers
 {
     public class ProductController : Controller
     {
-        public IActionResult Index()
+        private readonly ApplicationDbContext _context;
+        private readonly UserManager<ApplicationUser> _userManager;
+
+        public ProductController(ApplicationDbContext context, UserManager<ApplicationUser> userManager)
+        {
+            _context = context;
+            _userManager = userManager;
+        }
+
+        // GET: Farmers can see their own products
+        [Authorize(Roles = "Farmer")]
+        public async Task<IActionResult> MyProducts()
+        {
+            var user = await _userManager.GetUserAsync(User);
+            var products = await _context.Products
+                .Where(p => p.FarmerId == user.Id)
+                .ToListAsync();
+            return View(products);
+        }
+
+        // GET: Show product creation form
+        [Authorize(Roles = "Farmer")]
+        public IActionResult Create()
         {
             return View();
+        }
+
+        // POST: Save product
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        [Authorize(Roles = "Farmer")]
+        public async Task<IActionResult> Create(Product product)
+        {
+            if (!ModelState.IsValid)
+                return View(product);
+
+            var user = await _userManager.GetUserAsync(User);
+            product.FarmerId = user.Id;
+            _context.Products.Add(product);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(MyProducts));
+        }
+
+        // GET: Employee can view all products with filter
+        [Authorize(Roles = "Employee")]
+        public async Task<IActionResult> AllProducts(string category, DateTime? startDate, DateTime? endDate)
+        {
+            var query = _context.Products.Include(p => p.Farmer).AsQueryable();
+
+            if (!string.IsNullOrEmpty(category))
+            {
+                query = query.Where(p => p.Category == category);
+            }
+
+            if (startDate.HasValue)
+            {
+                query = query.Where(p => p.ProductionDate >= startDate);
+            }
+
+            if (endDate.HasValue)
+            {
+                query = query.Where(p => p.ProductionDate <= endDate);
+            }
+
+            var products = await query.ToListAsync();
+            return View(products);
         }
     }
 }
