b69339c8c29350cc7ae03f6d61ad48c1a57fc75a
Saihil Gurupersad <ST10294145@vcconnect.edu.za>
2025-06-20 14:07:03 +0200

    Updated Model

--- Models/Product.cs
+++ Models/Product.cs
@@ -1,23 +1,26 @@
-﻿using System;
+﻿using AgriEnergyConnect.Models;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
-namespace AgriEnergyConnect.Models
+public class Product
 {
-    public class Product
-    {
-        [Key]
-        public int ProductId { get; set; }
+    public int ProductId { get; set; }
 
-        [Required]
-        [StringLength(100)]
-        public string Name { get; set; }
+    [Required]
+    [StringLength(100)]
+    public string Name { get; set; }
 
-        [Required]
-        [StringLength(50)]
-        public string Category { get; set; }
+    [Required]
+    [StringLength(50)]
+    public string Category { get; set; }
 
-        [Required]
-        [DataType(DataType.Date)]
-        public DateTime ProductionDate { get; set; }
-    }
+    [Required]
+    public DateTime ProductionDate { get; set; }
+
+    // Foreign Key
+    [Required]
+    public string FarmerId { get; set; }
+
+    [ForeignKey("FarmerId")]
+    public virtual ApplicationUser Farmer { get; set; }
 }
