6d6427d5aad3e69be0178f4f6a594d72857a4b47
Saihil Gurupersad <ST10294145@vcconnect.edu.za>
2025-06-20 15:24:49 +0200

    Added missing namespace

--- Models/Product.cs
+++ Models/Product.cs
@@ -1,26 +1,29 @@
-﻿using AgriEnergyConnect.Models;
+﻿using System;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
-public class Product
+namespace AgriEnergyConnect.Models
 {
-    public int ProductId { get; set; }
+    public class Product
+    {
+        public int ProductId { get; set; }
 
-    [Required]
-    [StringLength(100)]
-    public string Name { get; set; }
+        [Required]
+        [StringLength(100)]
+        public string Name { get; set; }
 
-    [Required]
-    [StringLength(50)]
-    public string Category { get; set; }
+        [Required]
+        [StringLength(50)]
+        public string Category { get; set; }
 
-    [Required]
-    public DateTime ProductionDate { get; set; }
+        [Required]
+        public DateTime ProductionDate { get; set; }
 
-    // Foreign Key
-    [Required]
-    public string FarmerId { get; set; }
+        // Foreign Key
+        [Required]
+        public string FarmerId { get; set; }
 
-    [ForeignKey("FarmerId")]
-    public virtual ApplicationUser Farmer { get; set; }
+        [ForeignKey("FarmerId")]
+        public virtual ApplicationUser Farmer { get; set; }
+    }
 }
