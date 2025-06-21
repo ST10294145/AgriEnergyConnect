81ad4aa41723e6cf42e00c582141073613105083
Saihil Gurupersad <ST10294145@vcconnect.edu.za>
2025-06-21 15:23:22 +0200

    Add-Migration AddFarmerIdToProduct

---
+++ Migrations/20250621132238_AddFarmerIdToProduct.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace AgriEnergyConnect.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddFarmerIdToProduct : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "FarmerId",
+                table: "Products",
+                type: "nvarchar(450)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Products_FarmerId",
+                table: "Products",
+                column: "FarmerId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Products_AspNetUsers_FarmerId",
+                table: "Products",
+                column: "FarmerId",
+                principalTable: "AspNetUsers",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Products_AspNetUsers_FarmerId",
+                table: "Products");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Products_FarmerId",
+                table: "Products");
+
+            migrationBuilder.DropColumn(
+                name: "FarmerId",
+                table: "Products");
+        }
+    }
+}
