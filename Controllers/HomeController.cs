7180e73a70447b67e0fadb6706b8384d53165967
Saihil Gurupersad <ST10294145@vcconnect.edu.za>
2025-06-24 19:39:57 +0200

    Added section for about us in controller

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -41,5 +41,10 @@ namespace AgriEnergyConnect.Controllers
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
+
+        public IActionResult AboutUs()
+        {
+            return View();
+        }
     }
 }
